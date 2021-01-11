b4b34a403eac66f36dffde38968e326fc793d714
Pyae Sone <mgmgpyaesonewin@gmail.com>
2021-01-11 10:13:08 +0630

    Update & Delete User API updated

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    const users = this.usersService.getUsers();
+    return users;
+  }
+
+  @Get(':id')
+  getUser(@Param('id') userId: string) {
+    const user = this.usersService.getUser(userId);
+    return user;
+  }
 
   @Post()
   async addUser(
@@ -13,6 +32,26 @@ export class UsersController {
     const generatedId = await this.usersService.saveUser(username, password);
     return {
       id: generatedId,
+    };
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
+    await this.usersService.updateUser(id, username, password);
+    return {
+      message: 'Updated Successfully',
+    };
+  }
+
+  @Delete(':id')
+  async deleteIndustry(@Param('id') industryId: string) {
+    await this.usersService.deleteUser(industryId);
+    return {
+      message: 'Delete Successfully',
     };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -10,6 +10,19 @@ export class UsersService {
     @InjectModel('User') private readonly userModel: Model<UserDocument>,
   ) {}
 
+  async getUsers() {
+    const users = await this.userModel.find().exec();
+    return users;
+  }
+
+  async getUser(userId: string) {
+    const user = await this.findUser(userId);
+    return {
+      id: user.id,
+      username: user.username,
+    };
+  }
+
   async saveUser(username: string, password: string) {
     const newUser = new this.userModel({
       username,
@@ -17,5 +30,33 @@ export class UsersService {
     });
     const result = await newUser.save();
     return result.id as string;
+  }
+
+  async updateUser(userId: string, username: string, password: string) {
+    const updateUser = await this.findUser(userId);
+    if (username) {
+      updateUser.username = username;
+    }
+    if (password) {
+      updateUser.password = password;
+    }
+    updateUser.save();
+  }
+
+  async deleteUser(userId: string) {
+    await this.userModel.deleteOne({ _id: userId }).exec();
+  }
+
+  private async findUser(userId: string): Promise<UserDocument> {
+    let industry;
+    try {
+      industry = await this.userModel.findById(userId).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find industry');
+    }
+    if (!industry) {
+      throw new NotFoundException('Could not find industry');
+    }
+    return industry;
   }
 }
